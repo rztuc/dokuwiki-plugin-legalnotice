3ddc63d72b436164dacd80b7ea4f6f94dbfbeeb6
einhirn <marg@rz.tu-clausthal.de>
2014-01-31 08:24:37 +0100

    code cleanup for publication

--- action.php
+++ action.php
@@ -3,24 +3,13 @@
   if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
   require_once(DOKU_PLUGIN.'action.php');
 
-  class action_plugin_legalnoticelogin extends DokuWiki_Action_Plugin {
-
-    function getInfo(){
-      return array(
-        'author' => 'Christian Marg',
-        'email'  => 'marg@rz.tu-clausthal.de',
-        'date'   => '2009-06-05',
-        'name'   => 'legalnoticelogin',
-        'desc'   => 'adds a legal (or other) notice below the login form',
-        'url'    => 'http://www.dokuwiki.org/plugin:ondeniedlogin',
-      );
-    }
+  class action_plugin_legalnotice extends DokuWiki_Action_Plugin {
 
   function register(&$controller) {
       $controller->register_hook('TPL_ACT_RENDER', 'AFTER',  $this, 'legalnoticelogin');
     }
 
-    function legalnoticelogin(&$event, $param) {
+    function legalnotice(&$event, $param) {
 	global $conf,$ACT;
 
 	if($ACT!='login') return;
